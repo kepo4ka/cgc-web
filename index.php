0ddd3dfaaca5973e205c6a3be97da1a5c3b5045c
kepo4ka <konstantin.bugulov@mail.ru>
2018-04-20 23:22:27 +0300

    изменение заголовка

--- index.php
+++ index.php
@@ -13,7 +13,10 @@ require_once("core/head.php");
       <div class="mui--appbar-height"></div>
       <br>
       <br>
-      <div class="mui--text-display3 row wow zoomInDown">Brand.io</div>
+      <div class="mui--text-display3 row wow zoomInDown">
+        NOSU Code Game Challange</div>
+      <div class="mui--text-display5 row wow fadeIn">
+        Конкурс игровых стратегий</div>
       <br>
       <br>
       <button class="mui-btn mui-btn--raised" id="get_started_btn">Участвовать</button>
@@ -26,7 +29,7 @@ require_once("core/head.php");
   ?>
     <script>
       new WOW().init();
-      
+
     </script>
   </body>
 </html>
