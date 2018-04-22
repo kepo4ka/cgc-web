a08c40ebf48a300a8c16a2ff42c27e2aa22064d2
kepo4ka <konstantin.bugulov@mail.ru>
2018-04-22 20:34:27 +0300

    ошибка 404, 403

---
+++ error.php
@@ -0,0 +1,32 @@
+
+<!doctype html>
+<html>
+<?php
+require_once("core/head.php");
+?>
+<body>
+<?php
+require_once("core/header.php");
+?>
+
+
+<div id="content-wrapper" class=" mui-container-fluid">
+
+    <div class="mui-container-fluid mui-panel">
+        <h1>СТРАНИЦА НЕ НАЙДЕНА</h1>
+
+        <a href="/">Вернуться на главную</a>
+    </div>
+</div>
+
+
+<?php
+require_once("core/footer.php");
+?>
+
+<script>
+    new WOW().init();
+</script>
+</body>
+</html>
+
