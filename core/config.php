966199abae0c813d295b2c11da43ded94e32cb03
kepo4ka <konstantin.bugulov@mail.ru>
2018-04-22 20:36:33 +0300

    переменные задержки, имён файлов визуализации

--- core/config.php
+++ core/config.php
@@ -1,8 +1,13 @@
 <?php
-
+define("CGC", true);
 define("SOURCE_PATH", "sources");
 define("SANDBOX_GAMES_PATH", "sandbox");
 define("SOURCE_FILE_NAME", "strategy.cs");
 define("EXE_FILE_NAME", "Program.exe");
+define("GAMESTATES_DAT_FILE_NAME", "Visualizer.dat");
+define("GAMESTATES_JSON_FILE_NAME", "Visualizer.json");
+
+define("UPLOAD_TIME_OUT", 10);
+define("SANDBOX_CREATE_TIME_OUT", 20);
 
 ?>
